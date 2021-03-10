c21deef49dd589aab4f0d40e28f752259c1ef8db
Jidlafe Hegner <jidlafehegner@gmail.com>
2021-03-09 23:30:07 -0300

    modulo filesystem e event emitter

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,21 @@
-const log = require('./logger');
+// const log = require('./logger');
+// const fiu = require('./fileutil')
 
-console.log(log);
+// console.log(log);
+// log('Um teste')
 
-log('Um teste')
+// console.log(fiu.)
+
+// const fs = require('fs')
+
+// const files = fs.readdirSync('./')
+// console.log(files)
+
+const EventEmitter = require('events')
+const emitter = new EventEmitter()
+
+emitter.addListener("messageLogged", (arg) => {
+    console.log('Evento emitido', arg)
+})
+
+emitter.emit('messageLogged', {id: 12345, url: 'http://mylabs.org'})
